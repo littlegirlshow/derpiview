b156669e1475d365344e202714ded8177fdbc480
Jeremy Coffield <palafx@gmail.com>
2019-02-20 22:59:33 +0000

    Use node-fetch

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
-const https = require('https')
+const fetch = require('node-fetch')
 
-module.exports.hello = async (request) => {
+module.exports.getPost = async (request) => {
   const postId = request.pathParameters.postId
   if(!postId.match(/^[0-9]+$/)) {
     return {
@@ -10,16 +10,7 @@ module.exports.hello = async (request) => {
   }
   const propertyName = request.pathParameters.size
 
-  const derpiApiResponse = await new Promise((resolve, reject) => https.get(`https:\/\/derpibooru.org\/${postId}.json`, resolve).on('error', reject))
-    .then(response => new Promise(resolve => {
-      var chunks = []
-      response.on('data', data => {
-        chunks.push(data)
-      })
-
-      response.on('end', () => resolve(Buffer.concat(chunks)))
-    }))
-    .then(JSON.parse)
+  const derpiApiResponse = await fetch(`https:\/\/derpibooru.org\/${postId}.json`).then((res) => res.json())
 
   const fileLocation = derpiApiResponse.representations[propertyName]
   if(!fileLocation) {
