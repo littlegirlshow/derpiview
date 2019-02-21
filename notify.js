b4b34f5269e4391b1a77115744e02bbee9acb56f
Jeremy Coffield <palafx@gmail.com>
2019-02-21 20:34:44 +0000

    Simplify fetching service constants

--- notify.js
+++ notify.js
@@ -2,39 +2,23 @@ const fetch = require('node-fetch')
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
 
-let serviceConstants = null
+const serviceConstants = s3.getObject({
+  Bucket: 'service-constants',
+  Key: 'serviceConstants.json'
+}).promise().then((data) => JSON.parse(data.Body.toString()))
 
-async function populateServiceConstants() {
-  if(!serviceConstants) {
-    const params = {
-      Bucket: 'service-constants',
-      Key: 'serviceConstants.json'
-    }
+module.exports.notify = async (event) => Promise.all(
+  event.Records.map(async (record) => {
+    if(record.eventName !== 'INSERT') return
     
-    serviceConstants = await s3.getObject(params, (err) => {
-      if (err) {
-        throw err
-      }
-    }).promise().then((data) => JSON.parse(data.Body.toString()));
-  }
-}
-
-module.exports.notify = async (event) => {
-  await populateServiceConstants()
-  
-  await Promise.all(
-    event.Records.map((record) => {
-      if(record.eventName !== 'INSERT') return
-      
-      const postId = record.dynamodb.Keys.priPart.S.match(/^derpiLink\|(.*)$/)[1]
-      
-      return fetch(serviceConstants.notificationWebhook, {
-        method: 'post',
-        body: JSON.stringify({
-          content: `Image posted in the room: https://derpibooru.org/${postId}`
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      })
+    const postId = record.dynamodb.Keys.priPart.S.match(/^derpiLink\|(.*)$/)[1]
+    
+    return fetch((await serviceConstants).notificationWebhook, {
+      method: 'post',
+      body: JSON.stringify({
+        content: `Image posted in the room: https://derpibooru.org/${postId}`
+      }),
+      headers: { 'Content-Type': 'application/json' }
     })
-  )
-}
+  })
+)
